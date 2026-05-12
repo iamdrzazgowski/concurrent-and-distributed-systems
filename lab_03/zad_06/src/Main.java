8c1b40d9ae17a7b3bce8db5c9412d05c6e1b78dd
iamdrzazgowski <iamdrzazgowski@gmail.com>
2026-05-12 18:23:48 +0200

    semaphore zad_06a

---
+++ lab_03/zad_06/src/Main.java
@@ -0,0 +1,73 @@
+import java.util.concurrent.Semaphore;
+
+class Filozof extends Thread {
+    private int id;
+    private Semaphore left;
+    private Semaphore right;
+    private Semaphore jadalnia;
+
+    public Filozof(int id, Semaphore left, Semaphore right, Semaphore jadalnia) {
+        this.id = id;
+        this.left = left;
+        this.right = right;
+        this.jadalnia = jadalnia;
+    }
+
+    private void myslenie() throws InterruptedException {
+        System.out.println("Filozof " + id +" mysli");
+        Thread.sleep(1000);
+    }
+
+    private void jedzenie() throws InterruptedException {
+        System.out.println("Filozof " + id +" je");
+        Thread.sleep(1000);
+    }
+
+    public void run(){
+        try{
+            while(true){
+                myslenie();
+
+                jadalnia.acquire();
+
+                left.acquire();
+                System.out.println("Filozof " + id + " bierze lewy widelec");
+
+                right.acquire();
+                System.out.println("Filozof " + id + " bierze prawy widelec");
+
+                jedzenie();
+
+                left.release();
+                right.release();
+                System.out.println("Filozof " + id + " odłożył widelce");
+
+                System.out.println("Filozof " + id + " skończył jeść");
+                jadalnia.release();
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Semaphore[] widelce = new Semaphore[5];
+
+        for(int i = 0; i < 5; i++){
+            widelce[i] = new Semaphore(1);
+        }
+
+        Semaphore jadalnia = new Semaphore(4);
+        Filozof[] filozofowie = new Filozof[5];
+
+        for(int i = 0; i < 5; i++){
+            Semaphore left = widelce[i];
+            Semaphore right = widelce[(i + 1) % 5];
+
+            filozofowie[i] = new Filozof(i, left, right, jadalnia);
+            filozofowie[i].start();
+        }
+    }
+}
