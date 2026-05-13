4b8b41d824d403ee928819f252f706d6d088b4df
iamdrzazgowski <iamdrzazgowski@gmail.com>
2026-05-13 13:18:37 +0200

    monitors zad_02a

--- lab_04/zad_02a/src/Main.java
+++ lab_04/zad_02a/src/Main.java
@@ -1,15 +1,87 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+class Arbiter {
+    private int liczba = 0;
+    private int czytelnicy = 0;
+
+    public synchronized void pisz(int id, int liczba) throws InterruptedException {
+        while(czytelnicy > 0){
+            wait();
+        }
+
+        this.liczba = liczba;
+        System.out.println("Pisarz zapisał: " + this.liczba);
+
+        notifyAll();
+    }
+
+    public synchronized void czytaj(int id){
+        czytelnicy++;
+        System.out.println("Czytelnik " + id + " przeczytał: " + this.liczba);
+        czytelnicy--;
+        notifyAll();
+    }
+}
+
+class Pisarz extends Thread{
+    private final Arbiter arbiter;
+    private final int id;
+    private int x = 0;
+
+    Pisarz(Arbiter arbiter, int id) {
+        this.arbiter = arbiter;
+        this.id = id;
+    }
+
+    public void run(){
+        while (true){
+            try {
+                Thread.sleep(10);
+                arbiter.pisz(id, x++);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
+
+class Czytelnik extends Thread{
+    private final Arbiter arbiter;
+    private final int id;
+
+
+    Czytelnik(Arbiter arbiter, int id) {
+        this.arbiter = arbiter;
+        this.id = id;
+    }
+
+    public void run(){
+        while (true){
+            try {
+                Thread.sleep(10);
+                arbiter.czytaj(id);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int liczbaPisarz = 2;
+        int liczbaCzytelnikow = 12;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Arbiter arbiter = new Arbiter();
+        Pisarz[] pisarze = new Pisarz[liczbaPisarz];
+        Czytelnik[] czytelnicy = new Czytelnik[liczbaCzytelnikow];
+
+        for(int i = 0; i < liczbaCzytelnikow; i++){
+            czytelnicy[i] = new Czytelnik(arbiter, i);
+            czytelnicy[i].start();
+        }
+
+        for(int i = 0; i < liczbaPisarz; i++){
+            pisarze[i] = new Pisarz(arbiter, i);
+            pisarze[i].start();
         }
     }
 }
